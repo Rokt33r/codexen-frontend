0892d125d6a6ceccc95746bddcf53a284c2c9bdc
Dick Choi <fluke8259@gmail.com>
2015-04-18 16:27:47 +0900

    tag states added

--- src/services/tags.js
+++ src/services/tags.js
@@ -1,19 +1,42 @@
 angular.module('codexen.services.tags')
     .factory('Tag', function(Config, $http){
 
-        var index = function(search){
+        var index = function(search, limit){
 
             var url = Config.rootUrl + 'tags';
 
             var params = {};
 
             if(angular.isString(search)) params.search = search;
+            params.limit = limit;
 
             return $http.get(url, {params:params});
         };
 
+        var show = function(tag_name){
+            var url = Config.rootUrl + 'tags/' + tag_name;
+
+            return $http.get(url);
+        };
+
+        var cards = function(tag_name, page){
+            var url = Config.rootUrl + 'tags/' + tag_name + '/cards';
+
+            if(!page) page = 1;
+
+            var params = {
+                page:page
+            };
+
+            return $http.get(url, {
+                params:params
+            });
+        };
+
         return {
-            index:index
+            index:index,
+            show:show,
+            cards:cards
         };
 
     });
---
+++ src/states/tags/tags.show.js
@@ -0,0 +1,76 @@
+angular.module('codexen.states.tags.show')
+    .config(function($stateProvider){
+        $stateProvider.state('tags.show', {
+            url:'/:tag_name?{page:int}',
+            templateUrl:'states/tags/tags.show.tpl.html',
+            controller:'TagsShowController as vm',
+            params:{
+                page:1
+            }
+        });
+    })
+    .controller('TagsShowController', function($state, Tag, $scope, $timeout, $window){
+        var vm = this;
+
+        var tagName = $state.params.tag_name;
+        var page = $state.params.page;
+
+        Tag.show(tagName).success(function(data){
+            console.log(data.tag);
+            vm.tag = data.tag;
+
+            loadCards();
+
+        }).error(function(data, status){
+            console.log('Error !! ', status);
+            console.log('data : ', data);
+
+            $state.go('notfound');
+        });
+
+        vm.searchTags = function(){
+
+
+            Tag.index(vm.search, 30)
+                .success(function(data){
+                    console.log(data.tags);
+                    vm.searchedTags = data.tags;
+                })
+                .error(function(data, status){
+                    console.log('Error !! ', status);
+                    console.log('data : ', data);
+                });
+        };
+
+        vm.searchTags();
+
+        var loadCards = function(){
+            var currentOffset = $window.pageYOffset;
+            Tag.cards(tagName, page).success(function(data){
+                vm.cards = data.cards;
+
+                // redirect last page if current page is more than last page
+                if(page > vm.cards.last_page) $state.go('tags.show', {tag_name:tagName, page:vm.cards.last_page});
+
+                vm.currentPage = page;
+
+                vm.changePage = function(){
+
+                    console.log('Changed : ', vm.currentPage);
+
+                    $state.go('tags.show', {tag_name:tagName, page:vm.currentPage});
+
+                };
+
+                $timeout(function(){
+                    $window.scrollTo(0, currentOffset);
+                }, 0);
+            });
+        };
+
+        $scope.$on('cardDeleted', function(event, card){
+            event.preventDefault();
+            loadCards();
+        });
+
+    });
